12ab6533757c85eaec4d2ef2ff06460b06e4cfb9
xdaata <wordrow343@gmail.com>
2026-08-08 07:46:29 +0500

    Add: implement-prefix-tree - submission-0

---
+++ Data Structures & Algorithms/implement-prefix-tree/submission-0.py
@@ -0,0 +1,38 @@
+class TreeNode:
+    def __init__(self):
+        self.children = {}
+        self.is_end = False
+
+
+class PrefixTree:
+
+    def __init__(self):
+        self.root = TreeNode()
+        
+
+    def insert(self, word: str) -> None:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                node.children[char] = TreeNode()
+            node = node.children[char] 
+        node.is_end = True
+
+
+    def search(self, word: str) -> bool:
+        node = self.root
+        for char in word:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return node.is_end
+        
+    def startsWith(self, prefix: str) -> bool:
+        node = self.root
+        for char in prefix:
+            if char not in node.children:
+                return False
+            node = node.children[char]
+        return True
+        
+        
