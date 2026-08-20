f59989e44454892fd2bf7f6eeca3434efc2ae5c6
xdaata <wordrow343@gmail.com>
2026-08-20 20:52:50 +0500

    Add: reverse-integer - submission-1

---
+++ Data Structures & Algorithms/reverse-integer/submission-1.py
@@ -0,0 +1,17 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        res = 0
+        sign = -1 if x < 0 else 1
+        x = abs(x)
+
+        while x:
+            dig = x % 10
+            res = res * 10 + dig
+            x //= 10
+
+        res *= sign
+
+        if res < -2**31 or res > 2**31 - 1:
+            return 0
+        
+        return res
