4da00644b6373f1ccab6dec56b02c62c174c9249
xdaata <wordrow343@gmail.com>
2026-08-26 03:04:32 +0500

    Add: pow-x-n - submission-2

---
+++ Data Structures & Algorithms/pow-x-n/submission-2.py
@@ -0,0 +1,15 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        sign = -1 if n < 0 else 1
+        n = abs(n)
+        def recPow(x, n):
+            if x == 0: return 0
+            if n == 0: return 1
+            curr = recPow(x, n // 2)
+            curr *= curr
+            if n % 2 != 0:
+                curr *= x
+            return curr
+                
+        answ = recPow(x, n)
+        return answ if sign == 1 else 1/answ
